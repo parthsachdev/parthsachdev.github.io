81802b8a1535d0f2f6c550888d969643f98c19e0
parthsachdev <parthsachdev05@gmail.com>
2021-07-24 01:27:22 +0530

    new design: hero and navbar 2

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ async function startTitleAnimation () {
 	title.innerHTML = '';
 	for (var i=0; i<fullTitle.length; i++) {
 		title.innerHTML += fullTitle[i];
-		await wait(200);
+		await wait(150);
 	}
+	var subtitle = $('.hero .title h3');
+	subtitle.css('opacity', 1);
+	// works if display is none in css
+	// subtitle.fadeIn('slow');
 }
 
 async function wait (ms, value) {
